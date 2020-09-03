b029bc5b40f25d96a2c0d3868ff5cd7d6dc213b3
Alex Kuzko <okuzk@softserveinc.com>
2020-09-03 14:35:34 +0300

    Init Spring Boot application

---
+++ src/main/java/com/alexsoft/smarthouse/SmartHouseApplication.java
@@ -0,0 +1,12 @@
+package com.alexsoft.smarthouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHouseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartHouseApplication.class, args);
+    }
+
+}
