08c4ce2089bf73aae3f5d102fedc994c85e79f1d
Alex Kuzko <alex.kzko@gmail.com>
2024-12-13 22:34:49 -0500

    Power control

--- src/main/java/com/alexsoft/smarthouse/configuration/MqttConfiguration.java
+++ src/main/java/com/alexsoft/smarthouse/configuration/MqttConfiguration.java
@@ -1,12 +1,16 @@
 package com.alexsoft.smarthouse.configuration;
 
 import com.alexsoft.smarthouse.db.entity.Indication;
+import com.alexsoft.smarthouse.db.entity.IndicationV2;
+import com.alexsoft.smarthouse.db.entity.Measurement;
+import com.alexsoft.smarthouse.db.repository.IndicationRepositoryV2;
 import com.alexsoft.smarthouse.enums.AggregationPeriod;
 import com.alexsoft.smarthouse.service.IndicationService;
 import com.alexsoft.smarthouse.utils.DateUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.collections4.CollectionUtils;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,9 +27,13 @@ import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
 import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.scheduling.annotation.Scheduled;
 
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import static com.alexsoft.smarthouse.utils.Constants.S_OCEAN_DR_HOLLYWOOD;
@@ -36,6 +44,9 @@ public class MqttConfiguration {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MqttConfiguration.class);
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    public static final String MQTT_SMARTHOUSE_POWER_CONTROL_TOPIC = "mqtt.smarthouse.power.control";
+
+    private final IndicationRepositoryV2 indicationRepositoryV2;
 
     @Value("tcp://${mqtt.server}:${mqtt.port}")
     private String mqttUrl;
@@ -54,6 +65,14 @@ public class MqttConfiguration {
 
     private final DateUtils dateUtils;
     private final IndicationService indicationService;
+
+    private String powerState;
+//    private final IntegrationFlow mqttOutboundFlow;
+
+    public void sendMessage(String topic, String messagePayload) {
+        LOGGER.info("Sending MQTT message: topic={}, payload={}", topic, messagePayload);
+        mqttOutboundFlow().getInputChannel().send(MessageBuilder.withPayload(messagePayload).setHeader("mqtt_topic", topic).build());
+    }
 
     @Bean
     public DefaultMqttPahoClientFactory mqttClientFactory() {
@@ -104,6 +123,33 @@ public class MqttConfiguration {
                 LOGGER.error("Error processing MQTT message: {}", payload, e);
             }
         };
+    }
+
+    @Scheduled(cron = "0 0/1 * * * ?")
+    public void powerControl() {
+        List<IndicationV2> indications = indicationRepositoryV2.findByIndicationPlaceInAndLocalTimeIsAfter(List.of("APT2107S-MB", "APT2107S-B"),
+                LocalDateTime.now().minusMinutes(5));
+        if (CollectionUtils.isEmpty(indications)) {
+            powerState = "off";
+            sendMessage(MQTT_SMARTHOUSE_POWER_CONTROL_TOPIC, "{\"device\":\"AC\",\"state\":\"off\"}");
+            LOGGER.info("Power control method executed, indications were empty");
+        }
+        double ah = indications.stream().mapToDouble(i -> i.getAbsoluteHumidity().getValue()).average().orElseThrow();
+        LOGGER.info("Power control method executed, ah was: {}", ah);
+        if (ah > 12) {
+            powerState = "on";
+            sendMessage(MQTT_SMARTHOUSE_POWER_CONTROL_TOPIC, "{\"device\":\"AC\",\"state\":\"on\"}");
+        } else if (ah < 11) {
+            powerState = "off";
+            sendMessage(MQTT_SMARTHOUSE_POWER_CONTROL_TOPIC, "{\"device\":\"AC\",\"state\":\"off\"}");
+        }
+        Measurement humValue = new Measurement().setValue("on".equals(powerState) ? 10.0 : 0.0);
+        try {
+            indicationRepositoryV2.save(new IndicationV2().setIndicationPlace("APT2107S-HUM").setLocalTime(LocalDateTime.now())
+                    .setPublisherId("PI4").setInOut("IN").setAggregationPeriod("INSTANT").setTemperature(humValue).setAbsoluteHumidity(humValue));
+        } catch (Exception e) {
+            LOGGER.error("Error during saving humidity measurement: {}", humValue, e);
+        }
     }
 
     private void processIndication(Indication indication) {
--- src/main/java/com/alexsoft/smarthouse/db/repository/IndicationRepositoryV2.java
+++ src/main/java/com/alexsoft/smarthouse/db/repository/IndicationRepositoryV2.java
@@ -1,11 +1,15 @@
 package com.alexsoft.smarthouse.db.repository;
 
 import com.alexsoft.smarthouse.db.entity.IndicationV2;
+import org.springframework.data.domain.Limit;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface IndicationRepositoryV2 extends JpaRepository<IndicationV2, Integer> {
     List<IndicationV2> findByIdGreaterThan(Integer id);
     List<IndicationV2> findByAggregationPeriod(String aggregationPeriod);
+    List<IndicationV2> findByIndicationPlaceInAndLocalTimeIsAfter(List<String> indicationPlace, LocalDateTime localTime);
 }
