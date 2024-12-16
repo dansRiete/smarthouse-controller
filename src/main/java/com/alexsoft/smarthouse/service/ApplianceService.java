2e4241f29466ca495328fc42e7fb6566b013250e
Alex Kuzko <alex.kzko@gmail.com>
2024-12-16 16:50:46 -0500

    Save APT2107S-HUM to IndicationV1

--- src/main/java/com/alexsoft/smarthouse/service/ApplianceService.java
+++ src/main/java/com/alexsoft/smarthouse/service/ApplianceService.java
@@ -1,9 +1,10 @@
 package com.alexsoft.smarthouse.service;
 
-import com.alexsoft.smarthouse.db.entity.Appliance;
-import com.alexsoft.smarthouse.db.entity.IndicationV2;
-import com.alexsoft.smarthouse.db.entity.Measurement;
+import com.alexsoft.smarthouse.db.entity.*;
+import com.alexsoft.smarthouse.db.repository.IndicationRepository;
 import com.alexsoft.smarthouse.db.repository.IndicationRepositoryV2;
+import com.alexsoft.smarthouse.enums.AggregationPeriod;
+import com.alexsoft.smarthouse.utils.DateUtils;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 import static com.alexsoft.smarthouse.enums.ApplianceState.ON;
 
@@ -23,7 +26,9 @@ public class ApplianceService {
     public static final String MQTT_SMARTHOUSE_POWER_CONTROL_TOPIC = "mqtt.smarthouse.power.control";
 
     private final IndicationRepositoryV2 indicationRepositoryV2;
+    private final IndicationRepository indicationRepository;
     private final MqttService mqttService;
+    private final DateUtils dateUtils;
 
     public void switchAppliance(Appliance appliance, LocalDateTime localDateTime) {
         Long durationInMinutes = null;
@@ -41,10 +46,14 @@ public class ApplianceService {
     }
 
     private void saveAuxApplianceMeasurement(Appliance appliance, LocalDateTime localDateTime) {
-        Measurement humValue = new Measurement().setValue(appliance.getState() == ON ? 10.0 : 0.0);
+        double value = appliance.getState() == ON ? 10.0 : 0.0;
+        LocalDateTime utc = ZonedDateTime.now(ZoneId.of("UTC")).toLocalDateTime();
+        Measurement humValue = new Measurement().setValue(value);
         try {
-            indicationRepositoryV2.save(new IndicationV2().setIndicationPlace("APT2107S-HUM").setLocalTime(localDateTime)
+            indicationRepositoryV2.save(new IndicationV2().setIndicationPlace("DEHUMIDIFIER").setLocalTime(localDateTime)
                     .setPublisherId("PI4").setInOut("IN").setAggregationPeriod("INSTANT").setTemperature(humValue).setAbsoluteHumidity(humValue));
+            indicationRepository.save(Indication.builder().aggregationPeriod(AggregationPeriod.INSTANT).receivedUtc(utc).receivedLocal(localDateTime)
+                    .indicationPlace("DEHUMIDIFIER").air(Air.builder().temp(Temp.builder().celsius(value).build()).build()).build());
         } catch (Exception e) {
             LOGGER.error("Error during saving humidity measurement: {}", humValue, e);
         }
