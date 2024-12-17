221fdcad7588be991f2bf060ce7dde9b6520b81d
Alex Kuzko <alex.kzko@gmail.com>
2024-12-17 16:43:47 -0500

    Fix florida places and ah for dehumidifier

--- scripts/usefull-queries.sql
+++ scripts/usefull-queries.sql
@@ -1,1 +1,7 @@
 select avg(ah) from indication_v2 where indication_v2.indication_place in ('APT2107S-B', 'APT2107S-MB') and local_time > (select (NOW() - INTERVAL '5 hours') - INTERVAL '5 minutes');
+
+select ati.celsius, ati.ah
+from indication i
+         left join main.air a on i.air_id = a.id
+         left join main.air_temp_indication ati on a.temp_id = ati.id
+where i.indication_place = 'DEHUMIDIFIER' and aggregation_period = 'HOURLY' order by i.id desc ;
