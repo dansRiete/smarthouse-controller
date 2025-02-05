ba810552134c43dac08f775a86d997481cc0e602
Alex Kuzko <alex.kzko@gmail.com>
2025-02-04 21:59:06 -0500

    usefull-queries.sql update

--- scripts/usefull-queries.sql
+++ scripts/usefull-queries.sql
@@ -12,3 +12,15 @@ GROUP BY DATE(local_time) ORDER BY day desc;
 
 SELECT DATE(local_time) AS day, EXTRACT(HOUR FROM local_time) AS hour, COUNT(*) AS minutes_on FROM indication_v2 WHERE temp = 10
 AND indication_place = 'DEHUMIDIFIER' GROUP BY DATE(local_time), EXTRACT(HOUR FROM local_time) ORDER BY day DESC, hour DESC;
+
+-- KFXE TRAFFIC
+SELECT
+    (DATE_TRUNC('hour', timestamp - INTERVAL '5 hours') + INTERVAL '10 minutes' * FLOOR(EXTRACT(MINUTE FROM (timestamp + INTERVAL '5 hours')) / 10)) AS interval_start,
+    ROUND(AVG(airborne_aircrafts), 1) AS average_airborne_aircrafts,
+    airspace
+FROM
+    airspace_activity
+GROUP BY
+    interval_start, airspace
+ORDER BY
+    interval_start desc;
