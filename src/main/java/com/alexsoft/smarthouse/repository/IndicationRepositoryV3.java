99f6c06b216e0784b90bacf104686ba93ce6116d
Alex Kuzko <alex.kzko@gmail.com>
2025-09-27 14:29:51 -0400

    Add support for enhanced appliance control logic
    Implemented IndicationV3 support across services, introducing new measurement-based control logic and additional appliance attributes. Adjusted power control logic with improvements for handling hysteresis, locking, and MQTT messaging, ensuring flexibility for diverse scenarios. Updated scheduling and repository methods to incorporate new data and enhance functionality.
    
    (cherry picked from commit acc22fb233fb1bb98414796bf79b01d9c99c25c6)

--- src/main/java/com/alexsoft/smarthouse/repository/IndicationRepositoryV3.java
+++ src/main/java/com/alexsoft/smarthouse/repository/IndicationRepositoryV3.java
@@ -3,6 +3,9 @@ package com.alexsoft.smarthouse.repository;
 import com.alexsoft.smarthouse.entity.IndicationV3;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IndicationRepositoryV3 extends JpaRepository<IndicationV3, Integer> {
+import java.time.LocalDateTime;
+import java.util.List;
 
+public interface IndicationRepositoryV3 extends JpaRepository<IndicationV3, Integer> {
+    List<IndicationV3> findByDeviceIdInAndUtcTimeIsAfterAndMeasurementType(List<String> deviceIds, LocalDateTime localTime, String measurementType);
 }
