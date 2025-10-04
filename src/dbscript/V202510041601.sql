68a03eb9379cd723f8d47f22092292f555b33ddd
Alex Kuzko <alex.kzko@gmail.com>
2025-10-04 16:21:48 -0400

    Save last hour state in the database

---
+++ src/dbscript/V202510041601.sql
@@ -0,0 +1,7 @@
+CREATE TABLE hour_change_tracker (
+                                     id SERIAL PRIMARY KEY,
+                                     previous_hour INT NOT NULL,
+                                     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO hour_change_tracker (id, previous_hour, updated_at) VALUES (1, -1, now());
