3fc21722c12119a9aeed1885d851e31063855983
Alex Kuzko <alex.kzko@gmail.com>
2026-01-31 23:29:13 -0500

    Add AC heating mode

--- src/main/java/com/alexsoft/smarthouse/service/ApplianceService.java
+++ src/main/java/com/alexsoft/smarthouse/service/ApplianceService.java
@@ -81,8 +81,6 @@ public class ApplianceService {
 
                     //  control based ong avg setting
                     if (appliance.getSetting() != null) {
-                        boolean onCondition = average > appliance.getSetting() + appliance.getHysteresis();
-                        boolean offCondition = average < appliance.getSetting() - appliance.getHysteresis();
                         if (appliance.getCode().equals("AC") && appliance.getState() == OFF) {
                             Appliance fan = applianceRepository.findById("FAN").orElseThrow();
                             LocalDateTime now = toLocalDateTime(utc);
@@ -117,12 +115,25 @@ public class ApplianceService {
 
                             }
                         }
-                        if (Boolean.TRUE.equals(appliance.getInverted()) ? !onCondition : onCondition) {
-                            applianceFacade.toggle(appliance, ON, utc, "pwr-control", true);
-                            return;
-                        } else if (Boolean.TRUE.equals(appliance.getInverted()) ? !offCondition : offCondition) {
-                            applianceFacade.toggle(appliance, OFF, utc, "pwr-control", true);
-                            return;
+                        if (appliance.getCode().equals("AC") && Boolean.TRUE.equals(appliance.getInverted())) {
+                            // AC Heating mode
+                            if (average < appliance.getSetting() - appliance.getHysteresis()) {
+                                applianceFacade.toggle(appliance, ON, utc, "pwr-control", true);
+                                return;
+                            } else if (average > appliance.getSetting() + appliance.getHysteresis()) {
+                                applianceFacade.toggle(appliance, OFF, utc, "pwr-control", true);
+                                return;
+                            }
+                        } else {
+                            boolean onCondition = average > appliance.getSetting() + appliance.getHysteresis();
+                            boolean offCondition = average < appliance.getSetting() - appliance.getHysteresis();
+                            if (Boolean.TRUE.equals(appliance.getInverted()) ? !onCondition : onCondition) {
+                                applianceFacade.toggle(appliance, ON, utc, "pwr-control", true);
+                                return;
+                            } else if (Boolean.TRUE.equals(appliance.getInverted()) ? !offCondition : offCondition) {
+                                applianceFacade.toggle(appliance, OFF, utc, "pwr-control", true);
+                                return;
+                            }
                         }
 
                     }
